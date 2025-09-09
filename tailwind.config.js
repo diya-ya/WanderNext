167d6583f92c8a05c99000561994526441f1f75f
diya-ya <192265685+diya-ya@users.noreply.github.com>
2025-09-09 19:33:20 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          DEFAULT: '#0ea5e9',
+          dark: '#0284c7',
+          light: '#38bdf8'
+        }
+      },
+    },
+  },
+  plugins: [],
+};
+
+
